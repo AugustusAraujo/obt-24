acf66cbfea1fcf6117204c8ba7d55509de9ccb00
Augustus Oliveira <augustusaraujo13@gmail.com>
2024-07-22 16:00:53 -0300

    feat: foda

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <ion-app>
+  <ion-app class="one">
     <ion-router-outlet />
   </ion-app>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
 </script>
+
+<style>
+body,
+html {
+  background: white;
+  color: black;
+}
+</style>
