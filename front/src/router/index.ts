b454b719081838a2f475d70ecefd6375deb02983
Augustus Oliveira <augustusaraujo13@gmail.com>
2024-07-22 16:01:35 -0300

    fofda faodagiuenhe

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -4,7 +4,7 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    component: () => import("@/views/TabsPage.vue")
+    component: () => import("@/views/Home.vue")
   },
 ]
 
